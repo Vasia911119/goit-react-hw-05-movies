728718a2d5faf971c211bd80984111e7ee26930b
Vasyl Yakymets <Vasia911119@gmail.com>
2022-01-24 12:32:16 +0200

    Fix project  structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Container from './components/Container';
 import AppBar from './/components/AppBar';
@@ -7,26 +7,22 @@ import Loader from './components/Loader';
 const HomePage = lazy(() => import('./pages/HomePage/'));
 const MoviesPage = lazy(() => import('./pages/MoviesPage'));
 const MovieInfoPage = lazy(() => import('./pages/MovieInfoPage'));
-const Cast = lazy(() => import('./pages/Cast'));
-const Reviews = lazy(() => import('./pages/Reviews'));
+const Cast = lazy(() => import('./components/Cast'));
+const Reviews = lazy(() => import('./components/Reviews'));
 
 const App = () => {
   return (
     <Container>
       <AppBar />
-      {/* Роутинг приложения */}
       <Suspense fallback={Loader}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-
           <Route path="movies" element={<MoviesPage />} />
-
           <Route path="movies/:id" element={<MovieInfoPage />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-
-          <Route path="*" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
     </Container>
